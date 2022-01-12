a6e271b5f21759357995bd20cba947a47d14e1de
Rob Bertram <rbertram8@gmail.com>
2022-01-11 23:42:42 -0600

    add root error pages

---
+++ app/components/Error/index.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { Link } from "remix";
+import { match } from "ts-pattern";
+import { H1, P } from "~/library/components/Typography";
+
+interface ErrorProps {
+  type: "401" | "404" | "500s";
+}
+
+const Error: React.FC<ErrorProps> = ({ type }) => (
+  <>
+    <H1 center style={{ margin: "2rem" }}>
+      {match(type)
+        .with("404", () => "Page Not Found")
+        .with("401", () => "Not Authorized")
+        .otherwise(() => "Unexpected Error")}
+    </H1>
+    <Link prefetch="intent" to="/">
+      <P center style={{ marginBottom: "2rem" }}>
+        Go to Home Page
+      </P>
+    </Link>
+    <img
+      style={{ margin: "2rem auto", display: "block", maxWidth: "100%" }}
+      src={match(type)
+        .with("404", () => "/img/errors/404.svg")
+        .with("401", () => "/img/errors/401.svg")
+        .otherwise(() => "/img/errors/500s.svg")}
+    />
+  </>
+);
+
+type Error404Props = Omit<ErrorProps, "type">;
+const Error404: React.FC<Error404Props> = (props: Error404Props) => (
+  <Error {...props} type="404" />
+);
+type Error401Props = Omit<ErrorProps, "type">;
+const Error401: React.FC<Error401Props> = (props: Error401Props) => (
+  <Error {...props} type="401" />
+);
+
+type Error500sProps = Omit<ErrorProps, "type">;
+const Error500s: React.FC<Error500sProps> = (props: Error500sProps) => (
+  <Error {...props} type="500s" />
+);
+
+export { Error404, Error401, Error500s };
--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,16 @@
-import type { MetaFunction } from "remix";
+import React from "react";
 import {
   Links,
   LiveReload,
-  LoaderFunction,
   Meta,
+  MetaFunction,
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
+  useCatch,
 } from "remix";
+import { match } from "ts-pattern";
+import { Error401, Error404, Error500s } from "./components/Error";
 import styles from "./styles/global.css";
 import libraryStyles from "./styles/library.css";
 
@@ -22,29 +24,52 @@ export const meta: MetaFunction = () => ({
   description: "Learn to code with learners in Dallas",
 });
 
-export default function App() {
-  const data = useLoaderData();
+export const ErrorBoundary = ({ error }: { error: Error }) => {
+  console.error(error);
+  return (
+    <Document title="Error!">
+      <Error500s />
+    </Document>
+  );
+};
+
+export const CatchBoundary = () => {
+  const caught = useCatch();
+  console.log(caught);
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      {match(caught.status)
+        .with(401, () => <Error401 />)
+        .with(404, () => <Error404 />)
+        .otherwise(() => {
+          throw new Error(caught.data || caught.statusText);
+        })}
+    </Document>
+  );
+};
+
+const Document: React.FC<{ title?: string }> = ({ title, children }) => {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
       </head>
       <body>
-        <Outlet />
+        <React.StrictMode>{children}</React.StrictMode>
         <ScrollRestoration />
         <Scripts />
         {process.env.NODE_ENV === "development" && <LiveReload />}
-        {data && data.ENV && (
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `window.ENV = ${JSON.stringify(data.ENV)}`,
-            }}
-          />
-        )}
       </body>
     </html>
   );
-}
+};
+
+export default () => (
+  <Document>
+    <Outlet />
+  </Document>
+);
