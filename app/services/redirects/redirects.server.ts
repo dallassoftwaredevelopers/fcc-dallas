e08c0d44052fddccc11979cfb150e7146250186c
Rob Bertram <rbertram8@gmail.com>
2022-01-21 13:13:35 -0600

    fix redirect

--- app/services/redirects/redirects.server.ts
+++ app/services/redirects/redirects.server.ts
@@ -1,31 +1,40 @@
+import { pipe, pipeWith } from 'pipe-ts';
 import { redirect } from 'remix';
 import { REDIRECTS_MAP } from '~/constants/redirects';
 
-const httpsRedirect = (url: string) => {
-  if (url.includes('www')) {
-    let redirectUrl = url.replace('www.', '');
+const LOCAL_HOST = '127.0.0.1';
+const HTTP_PREFIX = 'http://';
+const HTTPS_PREFIX = 'https://';
+const HTTPS_WWW_PREFIX = 'https://www.';
 
-    if (!redirectUrl.includes('https') && redirectUrl.includes('http')) {
-      redirectUrl = redirectUrl.replace('http', 'https');
-    }
-    return redirect(redirectUrl, {
-      status: 301,
-    });
-  }
-  return null;
-};
+const httpToHTtps = (url: string) =>
+  url.includes(LOCAL_HOST) ? url : url.replace(HTTP_PREFIX, HTTPS_PREFIX);
 
-export const makeRedirectIfExists = (url: string) => {
-  const httpsRedirectResponse = httpsRedirect(url);
-  if (httpsRedirectResponse) {
-    return httpsRedirectResponse;
-  }
+const stripWww = (url: string) => url.replace(HTTPS_WWW_PREFIX, HTTPS_PREFIX);
 
-  const redirectTo = REDIRECTS_MAP[url];
-  if (!redirectTo) {
+const normalizeUrl = pipe(httpToHTtps, stripWww);
+
+const httpsRedirectIfNeeded = (url: string) => {
+  const redirectUrl = normalizeUrl(url);
+
+  if (redirectUrl === url) {
     return null;
   }
-  return redirect(redirectTo, {
+
+  return redirect(redirectUrl, {
     status: 301,
   });
+};
+
+// to avoid two redirect, create a format function for www and https -- so split up httpsRedirect
+export const makeRedirectIfExists = (url: string) => {
+  const redirectTo = REDIRECTS_MAP[url];
+  if (!redirectTo) {
+    return httpsRedirectIfNeeded(url);
+  }
+  return pipeWith(redirectTo, normalizeUrl, (r) =>
+    redirect(r, {
+      status: 301,
+    })
+  );
 };
